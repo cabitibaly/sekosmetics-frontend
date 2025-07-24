a9308be155d2cd9c1fe539b1d0f58e2e6885b676
cabitibaly <bitibalyclementalex@gmail.com>
2025-07-24 10:35:09 +0000

    page detail de categorie et de marque => done

---
+++ src/app/marques/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Footer from "@/components/accueil/footer";
+import MarqueDetailBody from "@/components/marque/marqueDetail/marqueDetailBody";
+import MarqueDetailHero from "@/components/marque/marqueDetail/marqueDetailHero";
+import Navbar from "@/components/navbar/navbar";
+import Topbar from "@/components/navbar/topbar";
+
+
+interface Props {
+    params: {
+        id: string
+    }
+}
+
+const MarqueDetail = async ({params}: Props) => {
+    const { id } = await params;
+
+    return (
+        <>
+            <Topbar href='/categories' title='Zaron' />
+            <Navbar isSidebarVisible={false} />
+            <MarqueDetailHero id={id} intitule={'Zaron'} />
+            <MarqueDetailBody />
+            <Footer />
+        </>
+    )
+}
+
+export default MarqueDetail
